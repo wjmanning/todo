80d0888344cda79cd34d6e4a12d9e6247aa13531
will manning <wjmanning@hotmail.com>
2018-05-22 10:48:33 +0100

    changed plain-text password in database connect config'

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 module.exports = {
-    remoteUrl : 'mongodb://remote:EWhZ66fp@ec2-54-83-143-176.compute-1.amazonaws.com:27017/dummyDB',
+    remoteUrl : 'mongodb://remote:test@ec2-54-83-143-176.compute-1.amazonaws.com:27017/dummyDB',
     localUrl: 'mongodb://localhost/meanstacktutorials'
 };
 
 // was remoteUrl : 'mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu'
-// and was 'mongodb://remote:EWhZ66fp@localhost:27017/dummyDB' 
+// and was 'mongodb://remote:test@localhost:27017/dummyDB' 
